c7898c75bc2878d3179d24a9da9d89b63f064ead
Ilyes <ilyeesx@gmail.com>
2026-06-11 03:19:20 +0200

    push

--- api/migrate.ts
+++ api/migrate.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { getSQL } from './_db'
+import { getSQL } from './_db.js'
 
 export default async function handler(_req: VercelRequest, res: VercelResponse) {
   const sql = getSQL()
